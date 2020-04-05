08b9e15a64456666da16be2678ded4c79adbff60
EliteKenn <53195969+EliteKenn@users.noreply.github.com>
2020-04-05 19:13:15 -0400

    Add files via upload

---
+++ EmailApp.java
@@ -0,0 +1,10 @@
+package EmailAdmin;
+import java.util.Scanner;
+public class EmailApp {
+public static void main(String[] args) {
+	
+	Email test = new Email("Martin", "Montanez");
+	System.out.println(test.showInfo());
+	
+}
+}
