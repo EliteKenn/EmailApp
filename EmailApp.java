74a2ea902f30fbffe72b9e5934a1a3a036ebc1f3
EliteKenn <53195969+EliteKenn@users.noreply.github.com>
2020-04-05 19:13:44 -0400

    Update EmailApp.java

--- EmailApp.java
+++ EmailApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class EmailApp {
 public static void main(String[] args) {
 	
-	Email test = new Email("Martin", "Montanez");
+	Email test = new Email("FirstName", "LastName");
 	System.out.println(test.showInfo());
 	
 }
